2092849a3dac3cd0150d523095c6595af52e216c
humbertoluis22 <humbertoluis2013@gmail.com>
2025-07-28 19:23:31 -0300

    modificando controller health

--- FIAP_Cloud_Games/FIAP_Cloud_Games/Controllers/HealthController.cs
+++ FIAP_Cloud_Games/FIAP_Cloud_Games/Controllers/HealthController.cs
@@ -2,11 +2,32 @@
 
 namespace FIAP_Cloud_Games.Controllers
 {
+    [Controller]
+    [Route("v1/[controller]")]
     public class HealthController : Controller
     {
-        public IActionResult Index()
+
+        /// <summary>
+        /// validar saude da aplicacao.
+        /// </summary>
+        /// <remarks>
+        /// Requisição pública para validar a saude da aplicacao.
+        /// Não exige autenticação.
+        /// </remarks>
+        /// <returns>Retorna uma mensagem simples.</returns>
+        /// <response code="200">Retorna uma mensagem simples.</response>
+        /// <response code="400">Erro na requisição</response>
+        [HttpGet]
+        public async Task<ActionResult> CheckHealth()
         {
-            return View();
+            try
+            {
+                return Ok(new { status = "Healthy" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
